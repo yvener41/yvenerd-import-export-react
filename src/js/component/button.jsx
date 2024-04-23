c97f94160d7951201e77f25c3bd875f52408a31f
Yvener Dutervil <72567854+yvener41@users.noreply.github.com>
2024-04-23 01:05:27 +0000

    myFile

---
+++ src/js/component/button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = () => {
+     return (
+         <button>I'm a button</button>
+     );
+}
+
+export default Button;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,38 @@
 import React from "react";
+import  Button from "./button";
+import Card from "./card";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+
+let person = {
+	  "name": "Yvener",
+}
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
+			<h1 className="text-center mt-5">Hello {
+				person.name === 'Yvener' ? person.name : "unknown"
+			}!</h1>
 			<p>
 				<img src={rigoImage} />
 			</p>
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working...
 			</a>
+			<div>
+				<Button />
+			</div>
+			<div>
+				<div className="row">
+					<div className="col-3"><Card /></div>
+					<div className="col-3"><Card /></div>
+					<div className="col-3"><Card /></div>
+					<div className="col-3"><Card /></div>
+				</div>
+			</div>
 			<p>
 				Made by{" "}
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
